cccaa42015fe2ac93d482faea014c5e0af6ba023
Manuel Restrepo <94997816+manuelRestrep0@users.noreply.github.com>
2023-11-03 16:09:03 -0500

    init

---
+++ src/main/java/com/manuel/proyecto/ProyectoEstructurasApplication.java
@@ -0,0 +1,13 @@
+package com.manuel.proyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoEstructurasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoEstructurasApplication.class, args);
+	}
+
+}
